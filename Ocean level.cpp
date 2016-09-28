9979034eb6736136d6fa09bc94e0c06986b97e1e
AdrianAlexander <dotakillingspree@yahoo.com>
2016-09-28 15:41:39 +0700

    Add files via upload

---
+++ Ocean level.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float Ocean_level=1.5;
+	int year = 1; 
+	float current;
+	
+	while (year<=25)
+	{
+		current = Ocean_level*year;
+		cout<<"result"<<current<<endl;
+		year++;
+			
+	}
+	
+	return 0;
+	
+}
